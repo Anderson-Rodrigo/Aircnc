2990b60739941a1093196c53b2d7705cdad94288
anderson <anderson.vargas90@gmail.com>
2019-10-06 18:44:44 -0300

    x

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+//import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg'
 
 function App() {
+  function handleSubmit(event){
+    event.preventDefault();
+
+    console.log('hell word')
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnc" />
@@ -12,7 +19,7 @@ function App() {
         <p>
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail *</label>
           <input 
             type="email" 
